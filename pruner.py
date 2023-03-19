a68d9b1e675d0b14dc816e8a7c2293a3567e4961
Jeanne11on <93199808+Jeanne11on@users.noreply.github.com>
2023-03-19 14:45:53 +0100

    Update pruner.py
    Added to the prune_tree function. I believe it's now functional.

--- pruner.py
+++ pruner.py
@@ -3,9 +3,10 @@ import numpy as np
 
 class Pruner:
     def __init__(self, tree, desired_num_leaves):
-        self.tree = tree
+        self.tree = tree.tree
         self.desired_num_leaves = desired_num_leaves
         self.num_leaves = 0
+        self.criterion = tree.criterion
 
     def catalog_twigs(self):
         twigs = []
@@ -32,21 +33,40 @@ class Pruner:
             self._count_leaves(node.right, leaves)
 
     def prune_tree(self):
+      '''For now, finds the twig with least impurity gain, deletes its children and transforms it into a leaf
+      '''
       twigs = self.catalog_twigs()
       if len(twigs) == 0:
-        return tree
+        return self.tree
 
-      # Calculate the impurity gain of each twig
-      impurity_gains = [twig.impurity_gain for twig in twigs]
-      # Find the index of the twig with the least impurity gain
-      min_index = np.argmin(impurity_gains)
-      # Remove the child node of the twig with the least impurity gain
-      twig = twigs[min_index]
-      twig.left = None
-      twig.right = None
+      # we get the number of leaves
+      self.num_leaves = self.count_leaves()
 
-      # Set the node's value to be the average of the labels of the samples in the subtree
-      twig.value = np.mean(node.samples.label)
+      while self.num_leaves > self.desired_num_leaves:
+        # Calculate the impurity gain of each twig
+        impurity_gains = [twig.impurity_gain for twig in twigs]
+        # Find the index of the twig with the least impurity gain
+        min_index = np.argmin(impurity_gains)
+        # Remove the child node of the twig with the least impurity gain
+        twig = twigs[min_index]
+        sample = [twig.left.value, twig.right.value]
+        twig.left = None
+        twig.right = None
+        twig.impurity_gain = 0
+
+        # Set the node's value to be the average of the labels of the samples in the subtree
+        if self.criterion == 'gini':
+            twig.value = np.max(sample)
+        elif self.criterion == 'mse':
+            twig.value = np.mean(sample)
+
+        # we update twigs based on the new tree
+        twigs = self.catalog_twigs()
+
+        # we update count_leaves based on the new tree
+        self.num_leaves = self.count_leaves()
+
+        #Todo: update the impurity and impurity gain of the nodes involved in this pruning 
 
 
 
